47007f893f8c3e75b0fc1a5cfc5a8b7d5c5ada70
Alain Poirier <alain.poirier@net-ng.com>
2018-02-19 16:41:28 +0100

    `local` request handler added

---
+++ nagare/local.py
@@ -0,0 +1,87 @@
+# --
+# Copyright (c) 2008-2018 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+"""Scoped objects
+
+Currently:
+
+  - objects scoped to a worker, a handler of one or several requests
+  - objects scoped to a request (i.e a scoped cleared on each new request)
+"""
+
+import threading
+
+from nagare.services import plugin
+
+
+class Thread(threading.local):
+    """Objects with attributes scoped to the current thread
+    """
+    def clear(self):
+        self.__dict__.clear()
+
+    @staticmethod
+    def create_lock():
+        return threading.Lock()
+
+
+class DummyLock(object):
+    acquire = release = lambda self: None
+
+
+class Process(object):
+    """Objects with attributes scoped to the process
+    """
+    def clear(self):
+        self.__dict__.clear()
+
+    @staticmethod
+    def create_lock():
+        return DummyLock()
+
+# ----------------------------------------------------------------------------
+
+
+class Local(plugin.Plugin):
+    LOAD_PRIORITY = 40
+
+    def __init__(self, name, dist, publisher_service):
+        global worker, request
+
+        super(Local, self).__init__(name, dist)
+
+        publisher = publisher_service.service
+
+        if publisher is not None:
+            worker = Thread() if publisher.has_multi_threads else Process()
+            request = Thread() if publisher.has_multi_threads else Process()
+
+    @property
+    def worker(self):
+        global worker
+        return worker
+
+    @property
+    def request(self):
+        global request
+        return request
+
+    @staticmethod
+    def handle_request(chain, **kw):
+        global request
+
+        request.clear()
+
+        return chain.next(**kw)
+
+# ----------------------------------------------------------------------------
+
+
+worker = None
+request = None
