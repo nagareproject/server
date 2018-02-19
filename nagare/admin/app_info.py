5d3705d51ba15c266997b59d5ec1b8d74d84ddf1
Alain Poirier <alain.poirier@net-ng.com>
2018-02-19 16:13:53 +0100

    More infos displayed

--- nagare/admin/app_info.py
+++ nagare/admin/app_info.py
@@ -31,11 +31,39 @@ class Info(command.Command):
             help='name of the service to display (can be specified multiple times)'
         )
 
-    def run(self, on, off, names, services_service):
+        parser.add_argument(
+            '-p', '--package', action='store_true',
+            help='Display the Python packages'
+        )
+
+        parser.add_argument(
+            '-v', '--version', action='store_true',
+            help='Display the Python package versions'
+        )
+
+        parser.add_argument(
+            '-m', '--module', action='store_true',
+            help='Display the Python modules'
+        )
+
+        parser.add_argument(
+            '-l', '--location', action='store_true',
+            help='Display the Python package locations'
+        )
+
+        parser.add_argument(
+            '-d', '--description', action='store_true',
+            help='Display the plugin description'
+        )
+
+    @staticmethod
+    def run(on, off, names, services_service, **columns):
+        activated_columns = {name for name, activated in columns.items() if activated}
+
         criterias = lambda services, name, _: (name in services) in (on, off)  # noqa: E731
 
         if names:
             criterias = lambda services, name, service, c=criterias: c(services, name, service) and (name in names)  # noqa: E731
 
-        services_service.display(criterias=criterias)
+        services_service.display(activated_columns=activated_columns, criterias=criterias)
         return 0
