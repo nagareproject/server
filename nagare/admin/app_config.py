48d1cf8d381840dd0c871aa47ab67d818d755343
Alain Poirier <alain.poirier@net-ng.com>
2019-02-01 17:33:01 +0100

    Comparaison between the configuration and the spec added

--- nagare/admin/app_config.py
+++ nagare/admin/app_config.py
@@ -8,18 +8,42 @@
 # --
 
 from fnmatch import fnmatchcase
+from collections import OrderedDict
+from itertools import chain, groupby
 
+from configobj import ConfigObj
+
+from nagare.services.config import Validator
 from nagare.admin import command
 
 
 class Config(command.Command):
-    DESC = 'display the services configuration'
-    WITH_STARTED_SERVICES = True
+    DESC = 'display the services configuration and extra informations'
 
     def set_arguments(self, parser):
         parser.add_argument(
             '-n', '--name', action='append', dest='names',
             help='name of the service to display (can be specified multiple times and wildchars are allowed)'
+        )
+
+        parser.add_argument(
+            '-c', '--config', action='store_false', dest='with_info',
+            help='only display the configuration'
+        )
+
+        parser.add_argument(
+            '-i', '--info', action='store_false', dest='with_config',
+            help='only display the extra informations'
+        )
+
+        parser.add_argument(
+            '--not-modified', action='store_true',
+            help='display the not modified configuration parameters (parameters with default value)'
+        )
+
+        parser.add_argument(
+            '--modified', action='store_true',
+            help='display the modified configuration parameters'
         )
 
         super(Config, self).set_arguments(parser)
@@ -29,14 +53,67 @@ class Config(command.Command):
         return any(fnmatchcase(name, pattern) for pattern in (names or ['*']))
 
     @classmethod
-    def run(cls, names, services_service):
-        services = [(name, service) for name, service in services_service.items() if cls.match_service(names, name)]
+    def compare(cls, validator, spec, config, not_modified, modified):
+        r = {}
+        for k, v in config.items():
+            check = spec.get(k)
+            if check is None:
+                if modified:
+                    r[k] = v
+                continue
 
+            if isinstance(v, dict):
+                v = cls.compare(validator, check, v, not_modified, modified)
+                if v:
+                    r[k] = v
+            else:
+                try:
+                    default = validator.get_default_value(check)
+                except KeyError:
+                    continue
+
+                if (modified and (v != default)) or (not_modified and (v == default)):
+                    r[k] = v
+
+        return r
+
+    @classmethod
+    def run(cls, names, with_config, with_info, not_modified, modified, services_service):
+        services = sorted((name, service) for name, service in services_service.items() if cls.match_service(names, name))
         if not services:
             print('<empty>')
             return 1
 
-        for i, (name, service) in enumerate(sorted(services)):
-            service.info()
+        config = OrderedDict((name, service.plugin_config) for name, service in services)
+
+        if with_config:
+            print('Configuration')
+            print('-------------')
+
+            if not_modified or modified:
+                spec = {name: dict(service.plugin_spec, activated='boolean(default=True)') for name, service in services}
+                config = cls.compare(Validator(), spec, config, not_modified, modified)
+
+            lines = ConfigObj(config).write()
+
+            for section, lines in groupby(lines, lambda l: l.lstrip().startswith('[')):
+                lines = chain([''], lines) if section else sorted(lines)
+                for line in lines:
+                    if not line.lstrip().startswith('_'):
+                        print(line)
+
+        if with_info:
+            for name, service in services:
+                info = '\n'.join(service.format_info())
+                if not info:
+                    continue
+
+                print('')
+                title = "Service '{}'".format(name)
+                print(title)
+                print('-' * len(title))
+                print('')
+
+                print(info)
 
         return 0
