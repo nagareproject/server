075106b761e7ee9cd72ad517359087205aa3d653
Alain Poirier <alain.poirier@net-ng.com>
2019-02-25 17:48:50 +0100

    Reload hook added

--- nagare/server/services.py
+++ nagare/server/services.py
@@ -25,6 +25,10 @@ class Services(services.Services):
         return self.handlers('handle_start')
 
     @property
+    def reload_handlers(self):
+        return self.handlers('handle_reload')
+
+    @property
     def interactive_handlers(self):
         return self.handlers('handle_interactive')
 
